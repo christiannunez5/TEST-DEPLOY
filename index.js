d27786c8c1e68c34dd5771ed1d3b30a9c27ba3d3
christiannunez5 <christiannunez775@gmail.com>
2024-04-18 20:52:16 +0800

    asdasd

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import express from 'express'
 import blogsRoutes from './routes/blogsRoutes.js'
-import usersRoutes from './routes/usersRoutes.js'
 import { config } from 'dotenv';
-import mongoose from 'mongoose';
-//
+
 config();
 const app = express();
 const port = process.env.PORT;
@@ -11,19 +9,7 @@ const port = process.env.PORT;
 app.use(express.json())
 
 app.use('/api/blogs', blogsRoutes)
-app.use('/api/users', usersRoutes)
 
-const startServer = async() => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("connected to mongo db")
-        app.listen(process.env.PORT, () => {
-            console.log(`server running on port ${process.env.PORT}`);
-        })
-    }
-    catch (error) {
-        console.log(error.message)
-    }
-}
-
-startServer();
+app.listen(port, () => {
+    console.log(`server running on port ${port}`)
+})
